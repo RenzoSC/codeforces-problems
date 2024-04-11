ccbfc87d0668aee57ff60734bdc44e4cf857bb85
RenzoSC <renzoc009@gmail.com>
2024-04-11 12:07:10 -0300

    2 more problems

---
+++ resolutions/41ATranslation.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    string inp;
+    string trans;
+    std::cin>>inp;
+    std::cin>>trans;
+    int inputL, transL;
+    inputL = inp.length();
+    transL = trans.length();
+    if(inputL>transL){
+        std::cout<<"NO";
+        return 0;
+    }
+    for (size_t i = 0; i < inputL; i++)
+    {
+        if(inp[i]!=trans[transL-i-1]){
+            std::cout<<"NO";
+            return 0;
+        }
+    }
+    std::cout<<"YES";
+    return 0;
+}
---
+++ resolutions/546ASoldierBananas.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    int k,n,w, total;
+    std::cin >>k>>n>>w;
+    //w*(w+1)/2 me da la sumatoria *k sería todo
+    //lo que le costaría comprar w bananas 
+    //siendo q la primera cuesta k
+    // k* 1 + k*2 + k*3 .. +k*w
+    total=k*w*(w+1)/2;
+    std::cout<< (total<=n?0:total-n); 
+    return 0;
+}
