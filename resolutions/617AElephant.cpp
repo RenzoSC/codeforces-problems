39a0c6e8e22ed75b69bb445d22ca5069aa0d3347
RenzoSC <renzoc009@gmail.com>
2024-04-11 11:17:45 -0300

    another exercise

---
+++ resolutions/617AElephant.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    int o, x;
+    std::cin >>x;
+    o = x / 5;
+    o+= x%5 == 0? 0:1;
+    std::cout<<o;
+    return 0;
+}
